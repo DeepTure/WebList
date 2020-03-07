192ee40b6b3e506f79b7ee24ec5ad3c08297db8d
MarckDJ <marckdj11@gmail.com>
2020-03-07 15:42:56 -0600

    Validacion hecha

---
+++ js/validacion.js
@@ -0,0 +1,23 @@
+function validar() {
+    //ponganle id="usuario" al usuario y id="contraseña" a la contraseña
+    let usuario=document.getElementById("usuario").value
+    let contraseña=document.getElementById("contraseña").value
+    //usuario Kevin
+    if(usuario=="Kevin_Jafet" && contraseña=="MoranDT01"){
+        alert("Hola "+usuario)
+    //usuario Marco
+    }else if(usuario=="Marco_Antonio" && contraseña=="MotaDT02"){
+        alert("Hola "+usuario)
+    //usuario Daniel
+    }else if(usuario=="Daniel_Cuellar" && contraseña=="BetancourtDT03"){
+        alert("Hola "+usuario)
+    //usuario De la O
+    }else if(usuario=="Cristopher_De_la_O" && contraseña=="FloresDT04"){
+        alert("Hola "+usuario)
+    //usuario Piña
+    }else if(usuario=="Leo_Yeudiel" && contraseña=="PiñaDT05"){
+        alert("Hola "+usuario)
+    }else{
+        alert("no reconocido 'version de prueba'")
+    }
+}
