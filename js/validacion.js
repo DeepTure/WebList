aee51dc4ef25fe393762e742f81bd39640f5308c
cdelaof26 <50687577+cdelaof26@users.noreply.github.com>
2020-03-07 21:19:54 -0600

    Update validacion.js
    Ya están todas las cuentas y re-ordenamiento de las cuentas por orden de integración a DT

--- js/validacion.js
+++ js/validacion.js
@@ -2,22 +2,14 @@ function validar() {
     //ponganle id="usuario" al usuario y id="contraseña" a la contraseña
     let usuario=document.getElementById("login").usuario.value
     let contraseña=document.getElementById("login").contraseña.value
-    //usuario Kevin
-    if(usuario=="Kevin_Jafet" && contraseña=="MoranDT01"){
-        alert("Hola "+usuario)
-    //usuario Marco
-    }else if(usuario=="Marco_Antonio" && contraseña=="MotaDT02"){
-        alert("Hola "+usuario)
-    //usuario Daniel
-    }else if(usuario=="Daniel_Cuellar" && contraseña=="BetancourtDT03"){
-        alert("Hola "+usuario)
-    //usuario De la O
-    }else if(usuario=="Cristopher_De_La_O" && contraseña=="FloresDT04"){
-        alert("Hola "+usuario)
-    //usuario Piña
-    }else if(usuario=="Leo_Yeudiel" && contraseña=="PiñaDT05"){
-        alert("Hola "+usuario)
-    }else{
-        alert("No reconocido 'version de prueba'")
+    var pass = 0;
+    if((usuario=="Kevin_Jafet" && contraseña=="MoranDT01") || (usuario=="Leo_Yeudiel" && contraseña=="PiñaDT02") || (usuario=="Daniel_Josias" && contraseña=="CuellarDT03") || (usuario=="Cristopher_De_La_O" && contraseña=="FloresDT04")  || (usuario=="Marco_Antonio" && contraseña=="MotaDT05") || (usuario=="Carlos_Saúl" && contraseña=="AlferezDT06")){
+        pass = 1;
     }
+    if (pass == 1){
+        alert("Hola "+usuario)
+    }
+    else{
+        alert("No reconocido 'version de prueba'")
+    } 
 }
