a45e9c446c4ab87bc8099c8e5246cd8b9643033a
MarckDJ <marckdj11@gmail.com>
2020-03-17 08:58:52 -0600

    profiles contraseña 40% terminada

--- JS/Validate.js
+++ JS/Validate.js
@@ -1,3 +1,11 @@
+//Patrones 100% intelektuales osi osi Uwu
+var patron_pw_min=/[a-z]+/
+var patron_pw_may=/[A-Z]+/
+var patron_pw_num=/[0-9]+/
+var patron_pw_sib=/[#$%&°¬]+/
+var patron_pw_val_car=/[A-Za-z0-9#$%&°¬]+/
+//No copiar marca registrada MarckDJ©
+
 function Log_In() {
     let usr = document.getElementById("LogIn").Account.value
     let passwd = document.getElementById("LogIn").Password.value
@@ -5,5 +13,27 @@ function Log_In() {
         alert("Hola "+ usr)
     }else{
         alert("No se encontro el usuario")
+    }
+}
+
+function changePassword() {
+    let pass1 = document.getElementById("password").pass1.value
+    let pass2 = document.getElementById("password").pass2.value
+    if(pass1!=pass2 || pass1==null || pass2==null || pass1=="" || pass2==""){
+        alert("Las contraseñas no coinciden")
+    }else{
+        if(pass1.lenght>=8){
+            alert("el texto es lo suficientemente largo")
+        }if(patron_pw_may.test(pass1)){
+            alert("el texto tiene almenos una mayuscula")
+        }if(patron_pw_min.test(pass1)){
+            alert("el texto tiene almenos una minuscula")
+        }if(patron_pw_num.test(pass1)){
+            alert("el texto tiene almenos un numero")
+        }if(patron_pw_sib.test(pass1)){
+            alert("el texto tiene almenos un simbolo")
+        }if(patron_pw_val_car.test(pass1)){
+            alert("el texto tiene caracteres validos")
+        }
     }
 }
