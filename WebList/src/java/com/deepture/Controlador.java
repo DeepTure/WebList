0c723b1fcd29151bf0e03bcdfdc80d27aa8527bf
jafet5757 <moranorozcokevinjafet@gmail.com>
2020-05-23 20:09:50 -0500

    se creo el proyecto jsp pero no esta listo para programar

---
+++ WebList/src/java/com/deepture/Controlador.java
@@ -0,0 +1,81 @@
+/*
+ *
+ */
+package com.deepture;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author DeepTure
+ */
+
+/*
+    Vistas = jsp
+    servlet = controlador
+    clase java = Modelo
+_____________________________
+    -!No hacer impresiones desde el controlador (a menos que sea para pruebas de errores)
+    -!Trabaja con limpieza y comenta todo el codigo
+    -No toques el codigo de los demás a menos que entiendas bien que hace  
+    -El archivo xml con el pool se encuentra en META-INF/context.xml \\\ Si necesitas modificarlo para usar tu usuario y contraseña
+        Pero no cambies el "type", "url" o "driver"
+*/
+@WebServlet(name = "Controlador", urlPatterns = {"/Controlador"})
+public class Controlador extends HttpServlet {
+    //Instanciamos una variable de tipo modelo; que tiene todos los metodos del modelo para hacer consultas
+    private Modelo modelo;
+    
+    //definir el datasource
+    @Resource(name="jdbc/Productos")
+    private DataSource miPool;
+    
+    
+    //con este metodo init(); instanciamos el objeto de tipo modelo y le pasamos como parametro el pool para que pueda hacer las conexiones a la bbdd
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try{
+            modelo = new Modelo(miPool);
+        }catch(Exception e){
+            throw new ServletException(e);
+        }
+    }
+    
+
+    
+    //Usar los metodos doGet o doPost segun el motodo para pasar parametrso de las vistas
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //aquí meter todo el codigo necesario, para las acciones PERO no ensuciar, 
+        //Solo manden a llamar metodos
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //aquí meter todo el codigo necesario, para las acciones PERO no ensuciar, 
+        //Solo manden a llamar metodos
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+    
+    // Aquí crear todos los metodos necesarios
+
+}
---
+++ WebList/src/java/com/deepture/Modelo.java
@@ -0,0 +1,26 @@
+package com.deepture;
+
+import java.sql.*;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author DeepTure
+ */
+public class Modelo {
+    
+    //instanciamos una variable de tipo DataSource para obtener el poolConnection
+    private DataSource origenData;
+    
+    //generamos el constructor 
+    public Modelo(DataSource origenData) {
+        this.origenData = origenData;
+    }
+    /*
+        Con el constructor obtenemos el pool desde el controlador, y para obtener la conexion con la base de datos
+        se ejecuta lo siguiente "Connection conexion = origenData.getConnection();" y en la variable conexion obtenemos obtenemos la 
+        conexion para acceder a la base de datos; En caso de tener dudas revisar el ejemplo que mandé, videos o preguntar
+    */
+
+    
+}
